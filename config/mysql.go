7b374aba384a075684a3779f127b44a41537b554
李智维 <13434615275@163.com>
2024-10-14 23:31:14 +0800

    feat: 添加数据库的相关配置（包括创建连接等）

---
+++ config/mysql.go
@@ -0,0 +1,87 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+)
+
+var DB *gorm.DB
+
+// 分页
+func Paginate(page int, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page == 0 {
+			page = 1
+		}
+
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
+
+func InitDB(conf *MysqlConfig) *gorm.DB {
+	username := conf.Username
+	password := conf.Password
+	host := conf.Host
+	port := conf.Port
+	database := conf.Database
+	maxIdleConns := conf.MaxIdleConns
+	maxOpenConns := conf.MaxOpenConns
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		username,
+		password,
+		host,
+		port,
+		database,
+	)
+	// gorm 日志配置
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second, // 慢 SQL 阈值
+			LogLevel:                  logger.Info, // 日志级别
+			IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  true,        // 彩色打印
+		},
+	)
+
+	// 打开数据库连接
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "ws_",
+			SingularTable: true, // 使用单数
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	// 连接池
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	sqlDB.SetMaxIdleConns(maxIdleConns) // 设置连接池中空闲连接的最大数量
+	sqlDB.SetMaxOpenConns(maxOpenConns) //设置打开数据库连接的最大数量
+	sqlDB.SetConnMaxLifetime(time.Hour) //设置了连接可复用的最大时间
+	DB = db
+	return db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
--- main.go
+++ main.go
@@ -9,4 +9,6 @@ func main() {
 	// 初始化配置
 	config.InitConfig("./config/config-dev.yaml")
 	fmt.Printf("Conf: %+v\n", config.Conf.MysqlConfig)
+	// 连接数据库
+	config.InitDB(config.Conf.MysqlConfig)
 }
