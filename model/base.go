a8878f9b233669a5cae145f19dc344dd779c9c9a
李智维 <13434615275@163.com>
2024-10-15 23:45:11 +0800

    feat: 创建基础 Model, 包括公共的字段和方法

---
+++ model/base.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type BaseModel struct {
+	Id         int32      `gorm:"column:id;primary_key" json:"id"`
+	CreateTime time.Time  `gorm:"column:create_time" json:"create_time"`
+	UpdateTime time.Time  `gorm:"column:update_time" json:"update_time"`
+	DeleteTime *time.Time `gorm:"column:delete_time" json:"delete_time"`
+}
+
+// gorm 钩子函数，用于创建时间和更新时间的自动赋值
+func (b *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
+	b.CreateTime = time.Now()
+	b.UpdateTime = time.Now()
+	return nil
+}
