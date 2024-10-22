7bb5276d847fa0e502a66815c0917ca7a4a5b2f7
李智维 <13434615275@163.com>
2024-10-22 23:01:44 +0800

    fix: swag init 生成接口文档报错
    原因：
    前面为了实现逻辑删除 DeleteTime 字段使用了 gorm.DeletedAt 类型，swagger 无法识别
    
    解决：
    在将结果转换为 JSON 时，将 DeleteTime 字段过滤掉(json:"-"), 应该前端使用不到该字段，后续使用到的话再想其他的方法，这里没必要提前优化！

--- model/base.go
+++ model/base.go
@@ -9,7 +9,7 @@ type BaseModel struct {
 	Id         int64          `gorm:"column:id;primary_key" json:"id"`
 	CreateTime time.Time      `gorm:"column:create_time" json:"create_time"`
 	UpdateTime time.Time      `gorm:"column:update_time" json:"update_time"`
-	DeleteTime gorm.DeletedAt `gorm:"column:delete_time" json:"delete_time"`
+	DeleteTime gorm.DeletedAt `gorm:"column:delete_time" json:"-"`
 }
 
 // gorm 钩子函数，用于创建时间和更新时间的自动赋值
